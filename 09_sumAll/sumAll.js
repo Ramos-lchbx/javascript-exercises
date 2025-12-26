93c83b14e0deb0dc9ca919a1b395fc0f5e4415f8
Ramos-lchbx <alejandroramos181298@hotmail.com>
2025-12-26 16:05:26 -0600

    Add solution to exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstVal, secondVal) {
+
+    if ( firstVal < 0 || secondVal < 0 ) return "ERROR";
+    if ( Number.isInteger(firstVal) === false || Number.isInteger(secondVal) === false ) return "ERROR";
+    if ( firstVal > secondVal ){
+        const wip = firstVal;
+        firstVal = secondVal;
+        secondVal = wip;
+    }
+
+    let sum= 0;
+
+    for (let i = firstVal ; i <= secondVal ; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
