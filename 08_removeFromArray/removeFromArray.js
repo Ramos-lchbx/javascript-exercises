aff0c8d93aed888fb447725588c489f32dac21c0
Ramos-lchbx <alejandroramos181298@hotmail.com>
2025-12-26 14:58:03 -0600

    Add solution to exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removedItems) {
+    for (let i = 0 ; i < arr.length ; i++){
+        if ( removedItems.includes(arr[i]) ){
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
