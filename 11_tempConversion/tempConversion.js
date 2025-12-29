ca204b8a8bec0d3ba4e55101ebcaf399e3a372f4
Ramos-lchbx <alejandroramos181298@hotmail.com>
2025-12-29 14:13:04 -0600

    Add solution to exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let converted = ((temp-32)/(9/5));
+    let readable = Math.round(converted * 10) / 10;
+    return readable;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    let converted = (temp * (9/5) + 32);
+    let readable = Math.round(converted * 10) / 10;
+    return readable;
 };
 
 // Do not edit below this line
