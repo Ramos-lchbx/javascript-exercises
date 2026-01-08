3741c67e05d47f2a1e73d44c74c08cd12d970292
Ramos-lchbx <alejandroramos181298@hotmail.com>
2026-01-08 15:56:18 -0600

    Add solution to exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [1, 1, 2, 3, 5]
+    
+    if ( num == 0 ) return 0;
 
+    else if ( num < 0 ) return "OOPS";
+
+    else {
+        for( let i = 5 ; i < num ; i++ ){
+        fib.push( fib[ i - 1 ] + fib[ i - 2 ] )
+        }
+
+        return fib[ num - 1 ];
+    }
+    
 };
 
 // Do not edit below this line
