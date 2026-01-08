b8d93ecd76882d2d31f6756f79da013e7473ba02
Ramos-lchbx <alejandroramos181298@hotmail.com>
2026-01-08 12:57:56 -0600

    Add solution to exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let letters = str.split("");
+    let lettersRev = letters
+        .slice()
+        .reverse()
+        .join()
+        .replace(/[^a-zA-Z0-9]/g, '')
+        .toLowerCase();
+    letters = letters
+        .join()
+        .replace(/[^a-zA-Z0-9]/g, '')
+        .toLowerCase();
+    if(letters === lettersRev){
+        return true;
+    }else return false;
 };
 
 // Do not edit below this line
