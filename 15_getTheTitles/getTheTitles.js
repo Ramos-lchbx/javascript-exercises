55f2b0741ab34401b9b2f5e6b48e2f481a6fdb10
Ramos-lchbx <alejandroramos181298@hotmail.com>
2026-01-12 11:30:27 -0600

    Add solution to exercise 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titles = arr.reduce((acc, val) => {
+        acc.push(val.title);
+        return acc;
+    }, [] )
+    return titles;
 };
 
 // Do not edit below this line
