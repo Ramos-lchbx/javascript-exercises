b9ab24cd00105f580e74d980319270185e2f2c36
Ramos-lchbx <alejandroramos181298@hotmail.com>
2026-01-12 13:53:14 -0600

    Add solution to exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldest = array.reduce((acc, val) => {
+        today = new Date();
 
+        if (!acc.yearOfDeath){
+            acc.yearOfDeath = today.getFullYear();
+        }
+
+        let accAge = acc.yearOfDeath - acc.yearOfBirth;
+        let valAge = val.yearOfDeath - val.yearOfBirth;
+        
+        if ((valAge) > (accAge)){
+            return val;
+        }  
+        return acc;
+    })
+    return oldest;
 };
 
-// Do not edit below this line
+// Do not edit below this lines
 module.exports = findTheOldest;
